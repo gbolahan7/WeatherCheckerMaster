9165da74daa65f352bf1d06d496daf3660834eb2
Abass Durodola <adurodola@softalliance.co.ng>
2021-07-13 10:24:16 +0100

    Failed Authentication issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'login.dart';
+
+import 'screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,134 +19,36 @@ class MyApp extends StatelessWidget {
     800: Color.fromRGBO(20, 47, 78, .9),
     900: Color.fromRGBO(20, 47, 78, 1),
   };
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     MaterialColor colorCustom = MaterialColor(0xFF142F4E, color);
     return MaterialApp(
-      title: 'Weather App',
+      title: 'Flutter Authentication',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          primarySwatch: colorCustom,
-          accentColor: Colors.indigoAccent,
-          fontFamily: "Montserrat",
-          textTheme: TextTheme(bodyText1: TextStyle(color: Colors.black))),
-      home: MyHomePage(title: 'Weather Checker App'),
-      // home: WelcomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _auth = FirebaseAuth.instance;
-  bool showProgress = false;
-
-  late String email, password;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Weather Checker App"),
-      ),
-      body: Center(
-        child: ModalProgressHUD(
-          inAsyncCall: showProgress,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "SignUp Page",
-                style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                textAlign: TextAlign.center,
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                    hintText: "Enter your Email",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)))),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextField(
-                obscureText: true,
-                textAlign: TextAlign.center,
-                onChanged: (value) {},
-                decoration: InputDecoration(
-                    hintText: "Enter your Password",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(32.0)))),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Material(
-                elevation: 5,
-                color: Colors.lightBlue,
-                borderRadius: BorderRadius.circular(32.0),
-                child: MaterialButton(
-                  onPressed: () async {
-                    setState(() {
-                      showProgress = true;
-                    });
-                    try {
-                      final newUser =
-                          await _auth.createUserWithEmailAndPassword(
-                              email: email, password: password);
-                      if (newUser != null) {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => MyLoginPage()),
-                        );
-                        setState(() {
-                          showProgress = false;
-                        });
-                      }
-                    } catch (e) {}
-                  },
-                  minWidth: 200.0,
-                  height: 45.0,
-                  child: Text(
-                    "Register",
-                    style:
-                        TextStyle(fontWeight: FontWeight.w500, fontSize: 20.0),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MyLoginPage()),
-                  );
-                },
-                child: Text(
-                  "Already Registred? Login Now",
-                  style: TextStyle(
-                      color: Colors.blue, fontWeight: FontWeight.w900),
-                ),
-              )
-            ],
+        primarySwatch: colorCustom,
+        accentColor: Colors.indigoAccent,
+        fontFamily: "Montserrat",
+        brightness: Brightness.light,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            textStyle: TextStyle(
+              fontSize: 24.0,
+            ),
+            padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
           ),
         ),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 46.0,
+            color: Colors.blue.shade700,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyText1: TextStyle(fontSize: 18.0),
+        ),
       ),
+      home: LoginPage(),
     );
   }
 }
